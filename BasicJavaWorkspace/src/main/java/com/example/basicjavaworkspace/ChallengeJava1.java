ae76248af04779d40d00938996a3aeac95e5dc29
Eden Lukomsky <Edenlukomsky@gmail.com>
2025-09-30 18:59:28 +0300

    Added module BasicJavaWorkspace Completed Challenge0 Completed ChallengeJava1

---
+++ BasicJavaWorkspace/src/main/java/com/example/basicjavaworkspace/ChallengeJava1.java
@@ -0,0 +1,9 @@
+package com.example.basicjavaworkspace;
+
+public class ChallengeJava1 {
+    public static void main(String[] args) {
+        double celsius = 15.0;
+        double fahrenhiet = (celsius * 9/5) + 32;
+        System.out.println(celsius + "℃ is " + fahrenhiet + "℉");
+    }
+}
